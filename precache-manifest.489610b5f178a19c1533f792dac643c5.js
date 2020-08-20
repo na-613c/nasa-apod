d3823ee477c5c194ba66cc553b22e61a6d359614
Shavlovsii_Ivan <na613c613c@gmail.com>
2020-08-21 01:55:31 +0300

    Updates

--- precache-manifest.489610b5f178a19c1533f792dac643c5.js
+++ precache-manifest.489610b5f178a19c1533f792dac643c5.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1ce8eede3e77dbe0ea69f5d214d3d4ff",
+    "revision": "8dec41cbc95fd4e449cccd901e972c1f",
     "url": "/nasa-apod/index.html"
   },
   {
-    "revision": "d293ccb2b98afbf382a3",
+    "revision": "723f5e096ef53a20d764",
     "url": "/nasa-apod/static/css/2.5215f976.chunk.css"
   },
   {
-    "revision": "8f9bbe29182c67a98b7a",
-    "url": "/nasa-apod/static/css/main.369dc695.chunk.css"
+    "revision": "6ae5d10110d4a3a21110",
+    "url": "/nasa-apod/static/css/main.b26c3ade.chunk.css"
   },
   {
-    "revision": "d293ccb2b98afbf382a3",
-    "url": "/nasa-apod/static/js/2.01b26494.chunk.js"
+    "revision": "723f5e096ef53a20d764",
+    "url": "/nasa-apod/static/js/2.5fd5e795.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/nasa-apod/static/js/2.01b26494.chunk.js.LICENSE.txt"
+    "url": "/nasa-apod/static/js/2.5fd5e795.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "8f9bbe29182c67a98b7a",
-    "url": "/nasa-apod/static/js/main.d4ca3712.chunk.js"
+    "revision": "6ae5d10110d4a3a21110",
+    "url": "/nasa-apod/static/js/main.fc9413f1.chunk.js"
   },
   {
     "revision": "b8f4816e63a1b0a4536f",
